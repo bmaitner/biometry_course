7c0075966bdb9b1f95a6f4f6a8174b5faf96b5b6
Brian <bmaitner@gmail.com>
2025-09-04 11:30:29 -0400

    adding examples and updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+# Overview
+
 This repo contains materials related to a statistical ecology course I'm teaching. The course is based on Ben Bolker's Ecological Models and Data in R textbook (ISBN: 978-0691125220), and so the organization and topics covered are largely taken from that source. Any content here (aside from images that may be licensed otherwise) should be considered CC-BY, while code is licensed under an MIT license.
+
+## Useful Links
+
+RStudio Keyboard Shortcuts
+
+* https://support.posit.co/hc/en-us/articles/200711853-Keyboard-Shortcuts-in-the-RStudio-IDE
+
+Places to look for data
+
+* https://opentraits.org/
--- R_scripts/Chapter_2_examples.R
+++ R_scripts/Chapter_2_examples.R
@@ -1,11 +1,14 @@
 # Code for chapter 2 examples
 #############################
 
-# Load libraries
+# Load libraries ----------------------------------------------------------
+
 
 library(readxl)
+library(BIEN)
 
-# Reading in data locally (relative paths)
+# Reading in data locally (relative paths) --------------------------------
+
 
   # Avonet data (Bird morphology and ecology)
   
@@ -23,14 +26,131 @@ library(readxl)
     
     european_amphibs <- read_xlsx("data/European_amphibians/oo_32985.xlsx")
     
+    
+      # Remember, you can use ?read_xslx to check the function arguments and what they do
+    
+        ?read_xlsx
+    
     european_amphibs_v2 <- read_xlsx("data/European_amphibians/oo_32985.xlsx",
                                 skip = 3,
                                 sheet = 1,
                                 na = "DD")
-    
-# Reading in data locally (absolute paths) Note: this example won't work for you
-    
-    avonet <- read.csv("C:/Users/Brian Maitner/Desktop/current_projects/Statistical_ecology_course/data/Avonet/AVONET1_BirdLife.csv")
+
+# Reading in data locally (absolute paths) --------------------------------
     
     
+    #avonet_v2 <- read.csv("C:/Users/Brian Maitner/Desktop/current_projects/Statistical_ecology_course/data/Avonet/AVONET1_BirdLife.csv")
     
+
+# Reading in remotely -----------------------------------------------------
+
+    
+    avonet_v3 <- read.csv("https://github.com/bmaitner/Statistical_ecology_course/raw/refs/heads/main/data/Avonet/AVONET1_BirdLife.csv")
+
+    
+# Getting data from an API online -----------------------------------------
+
+    
+    # sabal_palmetto_occurrences <- BIEN_occurrence_species(species = "Sabal palmetto")
+    
+# R data types ------------------------------------------------------------
+
+  # Numeric    
+    class(0)
+    
+  # Integer  
+    class(1L) # The L tells R to treat it as an integer rather than numeric
+  
+  # Date
+    class(as.Date("2025-01-01"))
+    
+  # POSIXt (dates and times)
+    
+    class(Sys.time())
+    
+  # Character  
+    class("Optimus Prime")
+
+  # Logical
+    class(TRUE)
+    
+  # Factor
+    factor_example <- as.factor(c("A","B","C"))
+    class(factor_example)
+
+    
+# Accessing elements of an object -----------------------------------------
+
+    
+    # List
+      test_list <- as.list(c("A"="Optimus Prime","B"="Megatron"))
+      
+        class(test_list)
+      
+        test_list$A
+        test_list[[1]]
+        test_list[1]
+        test_list[["A"]]
+        test_list["A"]
+      
+      
+  # Named Vector
+        
+    test_named_vector <- c("A"="Optimus Prime","B"="Megatron")
+    
+      class(test_named_vector) #Note that it tells you "character", the class of the elements of the vector
+      
+      test_named_vector[[1]]
+      test_named_vector[1]
+      test_named_vector[["A"]]
+      test_named_vector["A"]
+      
+  # Unnamed vector    
+      
+      test_vector <- c("Optimus Prime","Megatron")
+      
+      class(test_vector) #Note that it tells you "character", the class of the elements of the vector
+      
+      test_vector[[1]]
+      test_vector[1]
+
+  # Data.frame
+      
+    test_dataframe <- data.frame(character = c("Optimus Prime","Megatron"),
+                                    faction = c("Autobot","Decepticon"))
+    
+    rownames(test_dataframe) <- c("A","B")
+    
+    class(test_dataframe)
+    
+    # Select a column
+      test_dataframe$character
+      test_dataframe[,1]
+      test_dataframe["character"]
+      test_dataframe[,"character"]
+      
+    # select a row
+      test_dataframe[1,]
+      test_dataframe["A",]
+      
+    # Select the first cell
+      test_dataframe[1,1]
+      test_dataframe["A","character"]
+
+            
+# Figuring out what's in an object ----------------------------------------
+
+  # Get the class of a particular column
+      class(test_dataframe$faction)
+      class(test_dataframe[,2])
+
+      
+  # Summary functions
+      str(test_dataframe) #shows structure
+      summary(test_dataframe) #provides a summary
+      table(test_dataframe) # provides a table showing counts of combinations
+      head(test_dataframe) # shows the first few rows of data
+
+# Dealing with NAs --------------------------------------------------------
+
+      
